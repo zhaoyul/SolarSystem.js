2128e5765dc8fa995e83fc99648087f1c3539261
Abner <zhcjtht@gmail.com>
2014-06-26 16:55:09 -0400

    html/js initialization

--- js/solarsystem.js
+++ js/solarsystem.js
@@ -1,3 +1,40 @@
 $(document).ready(function(){
-  $("#control" ).find("#slider").slider(); //create slider bar
+  //init html/js
+  //slider bar functions
+  $("#slider").slider(); //create slider bar
+  $("#slider").on("mouseenter",function(){ //show slider bar info when mouse over
+      $("#label").slideToggle();
+  });
+  $("#slider").on("mouseleave",function(){ //remove slider bar info when mouse leave
+      $("#label").slideToggle();
+  });
+  var speed = 0;
+  $("#slider").mousemove(function(){
+    speed = +$('#slider').slider("option", "value");
+    $("#speed_value").text("×"+speed);
+  });
+  //button click
+  var drawpath = false; // set if draw the path
+  $("#draw").click(function(){
+    if(drawpath == false){
+      drawpath = true;
+      $("#draw").text("Erase Path");
+    }
+    else {
+      drawpath = false;
+      $("#draw").text("Draw Path");
+    }
+  });
+  //canvas background
+  var canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+  var backimg = new Image();
+  backimg.src = "./img/space.jpg";
+  backimg.onload = function(){
+    ctx.drawImage(backimg,0,0);
+    };
+
+  //draw Solar system
+
+
 });
