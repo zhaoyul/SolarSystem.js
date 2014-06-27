8871b55ff5bb40565782cfca47fee2fc4768148c
Abner Chou <zhcjtht@gmail.com>
2014-06-27 00:00:01 -0400

    Basic heliocentric

--- js/solarsystem.js
+++ js/solarsystem.js
@@ -13,6 +13,7 @@ $(document).ready(function(){
     $("#speed_value").text("×"+speed);
   });
   //button clicked
+  var geocentric = false;
   var drawpath = false; // set if draw the path
   $("#draw").click(function(){
     if(drawpath == false){
@@ -24,38 +25,59 @@ $(document).ready(function(){
       $("#draw").text("Draw Path");
     }
   });
+  $("#system").click(function(){
+    if(geocentric == false){
+      geocentric = true;
+      $("#system").text("Geocentric");
+    }
+    else {
+      geocentric = false;
+      $("#system").text("Heliocentric");
+    }
+  });
   //canvas background
   var canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
-  var backimg = new Image();
-  backimg.src = "./img/space.jpg";
+  var backimg = document.getElementById("background");
 
   //draw Solar system
-  var planets = new Array(9);
-  var maximage_width = 10;
-  var maximage_heigth = 10;
-  var center_x = canvas.width/2 - maximage_width/2;
-  var center_y = canvas.height/2 - maximage_heigth/2;
+  var number_planet = 9;
+  var planets = new Array(number_planet);
+  var maximagesize = 15; // same width and heigth
+  var center_x = canvas.width/2 - maximagesize/2;
+  var center_y = canvas.height/2 - maximagesize/2;
+  var planet_distance = 30;
   var timer = 0;
+  var interval_time = 100;
+  var earth_year = 3000; // how long a earth year on this canvas, unit is ms
+  var orbit_portion = earth_year/interval_time;
   var cycle = [0.000001,0.24,0.62,1,1.88,11.86,29.46,84,165]; //first element should be zero, set to 0.000001 is to avoid divid by zero
   function drawing(){
-    ctx.clearRect ( 0 , 0 , 500 , 500 );
-    ctx.drawImage(backimg,0,0);
-    for(i = 0; i < 9; i++){
+    ctx.clearRect ( 0 , 0 , canvas.width , canvas.height );
+    ctx.drawImage( backimg , 0 , 0 , canvas.width , canvas.height );
+    for(i = 0; i < number_planet; i++){
       planets[i] = document.getElementById(i);
-      var movement = Math.PI*(1/30)*timer/cycle[i];
-      ctx.drawImage(planets[i],center_x+Math.cos(movement)*30*i,center_y+Math.sin(movement)*30*i, maximage_width, maximage_heigth);
-      if(drawpath == true){
+      var movement = Math.PI*(1/orbit_portion)*timer/cycle[i];
+      ctx.drawImage(planets[i],center_x+Math.cos(movement)*planet_distance*i,center_y+Math.sin(movement)*planet_distance*i, maximagesize, maximagesize);
+      if(drawpath == true){ //draw path if button clicked
         ctx.beginPath();
         ctx.lineWidth="1";
         ctx.strokeStyle="green";
-        ctx.arc(center_x+maximage_width/2 ,center_y+maximage_heigth/2,i*30,0,360,false);
+        ctx.arc(center_x+maximagesize/2 ,center_y+maximagesize/2,i*planet_distance,0,360,false);
         ctx.stroke();
       }
     }
     timer += 1*(speed+1);
   }
-  setInterval(function(){drawing()}, 100);
+ 
+	//start rolling
+  var myInterval = setInterval(function(){drawing()}, interval_time);
+  $("canvas").mouseover(function(){
+	clearInterval(myInterval);
+  });
+  $("canvas").mouseleave(function(){
+	myInterval = setInterval(function(){drawing()}, interval_time);
+  });
 
 
 
